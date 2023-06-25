6e41d444c0da3992b097ff0b4958db314204e34c
Ro0t25 <rotunno.gianluca98@gmail.com>
2023-06-25 17:00:34 +0200

    adattato il codice di acquis alle nuove funzioni di detect

--- acquis.py
+++ acquis.py
@@ -3,11 +3,13 @@ import cv2
 from naoqi import ALProxy
 import numpy as np 
 from PIL import Image
-
+import math
+import time
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 videoDevice = ALProxy('ALVideoDevice', "nao.local.", 9559)
+motion = ALProxy("ALMotion","nao.local.",9559)
 
 AL_kTopCamera = 1
 AL_kQVGA = 2           
@@ -16,14 +18,21 @@ captureDevice = videoDevice.subscribeCamera("scatvcaataa", 1,1, 13, 30)
 
 while True:
     message = socket.recv()
-
-
     if(message == "acquisisci"):
             result = videoDevice.getImageRemote(captureDevice)
-            videoDevice.releaseImage(captureDevice)
             im = Image.frombytes("RGB", (result[0], result[1]), result[6])
             cvim = np.array(im)
             frame_enc = cv2.imencode(".jpg", cvim)[1].tobytes()
             socket.send(frame_enc)
-    else:
-        break
+    if(message == "inizializzazione"):
+            names = ["HeadPitch"]
+            angle_rad = [math.radians(29)]
+            motion.setStiffnesses("Body", 1.0)
+            motion.setAngles(names, angle_rad, 0.1)
+            socket.send("fatto")
+    if(message == "alzaTesta"):
+            names = ["HeadPitch"]
+            angle_rad = [math.radians(0)]
+            motion.setStiffnesses("Body", 1.0)
+            motion.setAngles(names, angle_rad, 0.1)
+            socket.send("fatto")
